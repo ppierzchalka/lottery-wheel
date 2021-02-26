c885fd72afa09ba33350d2b0fa2f851918e56c94
Przemyslaw Pierzchalka <p.pierzchalka@vistex.com>
2021-02-26 01:43:47 +0100

    husky test commit

--- src/greetKenobi.ts
+++ src/greetKenobi.ts
@@ -2,6 +2,6 @@ import { greet } from './greet';
 
 export const greetKenobi = (target: HTMLDivElement): void => {
   const newElem = document.createElement('p');
-  newElem.innerText = greet('General Kenobi!');
+  newElem.innerText = greet('General Kenobi!!');
   target.appendChild(newElem);
 };
